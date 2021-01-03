76cfe3e00200a4262ae850ff5150e98ed291d105
Wataru <jm.68wk@gmail.com>
2021-01-03 07:20:46 +0000

    マイグレーションを実行した

--- db/migrate/20210103070733_create_knowledges.rb
+++ db/migrate/20210103070733_create_knowledges.rb
@@ -1,7 +1,9 @@
 class CreateKnowledges < ActiveRecord::Migration[5.2]
   def change
     create_table :knowledges do |t|
-
+      t.text    :title
+      t.text    :text
+      t.integer :user_id
       t.timestamps
     end
   end
